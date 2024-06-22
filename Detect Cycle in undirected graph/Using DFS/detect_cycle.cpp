7cc8c083495c8c448ccabb28d26a2c90eb1b271a
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-06-22 17:58:29 +0530

    Cycle Detection in Graphs using Depth-First Search.

--- Detect Cycle in undirected graph/Using DFS/detect_cycle.cpp
+++ Detect Cycle in undirected graph/Using DFS/detect_cycle.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
+bool dfs(vector<vector<int> >&adj,vector<int>&visited,int node,int parent){
+    visited[node]=1;
+    for(auto it:adj[node]){
+        if(visited[it]==0){
+            if(dfs(adj,visited,it,node)==true){
+                return true;
+            }
+        }
+        else if(it!=parent){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool findCycle(vector<vector<int> >&adj,int n,int m){
+    vector<int>visited(n+1,0);
+    return dfs(adj,visited,1,-1);
+}
  
 int main(){
     int n;
@@ -12,7 +32,21 @@ int main(){
     cin>>m;
 
     vector<vector<int> >adj(n+1);
-    
+    cout<<"Enter the edges : "<<endl;
+    for(int i=0;i<m;i++){
+        int first,second;
+        cin>>first>>second;
+        adj[first].push_back(second);
+        adj[second].push_back(first);
+    }
+
+    if(findCycle(adj,n,m)){
+        cout<<"Yes , there is cycle present in the graph."<<endl;
+    }
+    else{
+        cout<<"No, There is no cycle present in the graph."<<endl;
+    }
+
 
 return 0;
 }
