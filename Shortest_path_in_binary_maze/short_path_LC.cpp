0073b12cb0fb9925c357763a1b8c9a33d29e117f
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-07-09 01:43:08 +0530

    Finding the shortest path in a binary maze where moving in all 8 directions is allowed with the help of Dijkstra's algorithm.

--- Shortest_path_in_binary_maze/short_path_LC.cpp
+++ Shortest_path_in_binary_maze/short_path_LC.cpp
@@ -3,33 +3,44 @@
 #include<queue>
 using namespace std;
 
-int shortest_path(vector<vector<int> >&maze,int n,int m,pair<int,int>&src,pair<int,int>&dest){
+int shortest_path(vector<vector<int> >&maze,int n,int m){
     priority_queue<pair<int,pair<int,int> >, vector<pair<int,pair<int,int> > >,greater<pair<int,pair<int,int> > > >pq;
     vector<vector<int> >dist(n,vector<int>(m,INT_MAX));
+    pair<int,int>src;
+    src.first=0;
+    src.second=0;
+
+    pair<int,int>dest;
+    dest.first=n-1;
+    dest.second=m-1;
+
     pair<int,pair<int,int> > p;
-    p.first=0;
+    p.first=1;
     p.second=src;
-    dist[src.first][src.second]=0;
+    dist[src.first][src.second]=1;
     pq.push(p);
     while(!pq.empty()){
         pair<int,int>cell=pq.top().second;
         int dis=pq.top().first;
         pq.pop();
-        int dirX[]={0,+1,0,-1};
-        int dirY[]={+1,0,-1,0};
-        for(int i=0;i<4;i++){
-            int nr=cell.first + dirX[i];
-            int nc=cell.second + dirY[i];
-            if(nr==dest.first && nc==dest.second){
-                return dis+1;
+        int dirX[]={-1,0,+1};
+        int dirY[]={-1,0,+1};
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                int nr=cell.first + dirX[i];
+                int nc=cell.second + dirY[j];
+                if(nr==dest.first && nc==dest.second){
+                    return dis+1;
+                }
+                if(nr>=0 && nr<n && nc>=0 && nc<m && maze[nr][nc]==0 && (dis + 1 < dist[nr][nc])){
+                    dist[nr][nc]=dis+1;
+                    p.first=dist[nr][nc];
+                    p.second.first=nr;
+                    p.second.second=nc;
+                    pq.push(p);
+                }
             }
-            if(nr>=0 && nr<n && nc>=0 && nc<m && (dis + 1 < dist[nr][nc])){
-                dist[nr][nc]=dis+1;
-                p.first=dist[nr][nc];
-                p.second.first=nr;
-                p.second.second=nc;
-                pq.push(p);
-            }
+            
         }
     }
     return -1;
@@ -54,20 +65,7 @@ int main(){
         }
     }
 
-    pair<int,int>src;
-    cout<<"Enter the Source Location : ";
-    int r,c;
-    cin>>r>>c;
-    src.first=r;
-    src.second=c;
-
-    pair<int,int>dest;
-    cout<<"Enter the Destination Location : ";
-    cin>>r>>c;
-    dest.first=r;
-    dest.second=c;
-
-    int ans=shortest_path(maze,n,m,src,dest);
+    int ans=shortest_path(maze,n,m);
 
     cout<<"The shortest distance from the source location to the destination location will be : "<<ans<<endl;
 
