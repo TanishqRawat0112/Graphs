2f088b98b6fdb9fee9973271affea7eea6a9f675
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-07-29 22:17:56 +0530

    .

---
+++ Number of Distinct Islands-DSU/num_of_islands.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ 
+int main(){
+    int n;
+    cout<<"Enter the number of rows in matrix : ";
+    cin>>n;
+
+    int m;
+    cout<<"Enter the number of columns in matrix : ";
+    cin>>m;
+
+    vector<vector<int> >matrix()
+    
+
+
+return 0;
+}
